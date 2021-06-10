f645527e3bfeb9a1b88cbd6719bda94ad829c731
joonhwan-choi <joonhwan8764@naver.com>
2021-06-11 01:36:51 +0900

    First

---
+++ MLnetProject/ObjectDetection/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿
+namespace ObjectDetection
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBo1 = new System.Windows.Forms.PictureBox();
+            this.pictureBo2 = new System.Windows.Forms.PictureBox();
+            this.pictureBo3 = new System.Windows.Forms.PictureBox();
+            this.pictureBo4 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(50, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.InitialImage = null;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 356);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 0);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(50, 50);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(500, 300);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(50, 356);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(500, 0);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(1222, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 101);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "▲";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("맑은 고딕", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(1222, 459);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 101);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "▼";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(569, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 54);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(575, 345);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 54);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "/";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(569, 435);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 54);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "0";
+            // 
+            // pictureBo1
+            // 
+            this.pictureBo1.Location = new System.Drawing.Point(635, 50);
+            this.pictureBo1.Name = "pictureBo1";
+            this.pictureBo1.Size = new System.Drawing.Size(500, 300);
+            this.pictureBo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBo1.TabIndex = 10;
+            this.pictureBo1.TabStop = false;
+            // 
+            // pictureBo2
+            // 
+            this.pictureBo2.Location = new System.Drawing.Point(635, 356);
+            this.pictureBo2.Name = "pictureBo2";
+            this.pictureBo2.Size = new System.Drawing.Size(500, 0);
+            this.pictureBo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBo2.TabIndex = 11;
+            this.pictureBo2.TabStop = false;
+            // 
+            // pictureBo3
+            // 
+            this.pictureBo3.Location = new System.Drawing.Point(635, 50);
+            this.pictureBo3.Name = "pictureBo3";
+            this.pictureBo3.Size = new System.Drawing.Size(500, 300);
+            this.pictureBo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBo3.TabIndex = 12;
+            this.pictureBo3.TabStop = false;
+            // 
+            // pictureBo4
+            // 
+            this.pictureBo4.Location = new System.Drawing.Point(635, 356);
+            this.pictureBo4.Name = "pictureBo4";
+            this.pictureBo4.Size = new System.Drawing.Size(500, 0);
+            this.pictureBo4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBo4.TabIndex = 13;
+            this.pictureBo4.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label4.Location = new System.Drawing.Point(31, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1119, 697);
+            this.label4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label5.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(253, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 32);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Before";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.label7.Font = new System.Drawing.Font("맑은 고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(868, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 32);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "After";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1222, 332);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 67);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "사진추가";
+            this.button3.UseVisualStyleBackColor = true;
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1376, 749);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pictureBo4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBo2);
+            this.Controls.Add(this.pictureBo1);
+            this.Controls.Add(this.pictureBo3);
+            this.Controls.Add(this.label4);
+            this.Name = "Form1";
+            this.Text = "이미지 객체 검색";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBo4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBo1;
+        private System.Windows.Forms.PictureBox pictureBo2;
+        private System.Windows.Forms.PictureBox pictureBo3;
+        private System.Windows.Forms.PictureBox pictureBo4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MLnetProject/ObjectDetection/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+namespace ObjectDetection
+{
+    
+    public partial class Form1 : Form
+    {
+        public static int p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            if (p % 2 == 0)
+            {
+                pictureBox1.Visible = true;
+                pictureBox1.Height = 0;
+                pictureBox1.Top = 350;
+                pictureBox4.Height = 300;
+                pictureBox4.Top = 360;
+                pictureBox1.BringToFront();
+                pictureBox2.BringToFront();
+
+                pictureBo1.Visible = true;
+                pictureBo1.Height = 0;
+                pictureBo1.Top = 350;
+                pictureBo4.Height = 300;
+                pictureBo4.Top = 360;
+                pictureBo1.BringToFront();
+                pictureBo2.BringToFront();
+
+                pictureBox1.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p - 2]);
+                pictureBo1.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p - 2]);
+                if (p == 2)
+                {
+                    pictureBox4.Visible = false;
+                    pictureBo4.Visible = false;
+                }
+                p = p - 1;
+                label1.Text = p + "";
+                label3.Text = (p-1) + "";
+                for (int i = 0; i < 300; i++)
+                {
+                    pictureBox2.Height = pictureBox2.Height - (int)(i * 0.01);
+                    pictureBox2.Top = pictureBox2.Top - (int)(i * 0.001);
+
+                    pictureBo2.Height = pictureBo2.Height - (int)(i * 0.01);
+                    pictureBo2.Top = pictureBo2.Top - (int)(i * 0.001);
+                    Application.DoEvents();
+                }
+                for (int i = 0; i < 300; i++)
+                {
+                    pictureBox1.Height = (int)(pictureBox1.Height + (int)(i * 0.01));
+                    pictureBox1.Top = (int)(pictureBox1.Top - (int)(i * 0.01));
+
+                    pictureBo1.Height = (int)(pictureBo1.Height + (int)(i * 0.01));
+                    pictureBo1.Top = (int)(pictureBo1.Top - (int)(i * 0.01));
+                    Application.DoEvents();
+                }
+            }
+            else
+            {
+                Debug.Write("홀");
+                pictureBox3.Visible = true;
+                pictureBox3.Height = 0;
+                pictureBox3.Top = 350;
+                pictureBox2.Height = 300;
+                pictureBox2.Top = 360;
+                pictureBox3.BringToFront();
+                pictureBox4.BringToFront();
+
+                pictureBo3.Visible = true;
+                pictureBo3.Height = 0;
+                pictureBo3.Top = 350;
+                pictureBo2.Height = 300;
+                pictureBo2.Top = 360;
+                pictureBo3.BringToFront();
+                pictureBo4.BringToFront();
+
+                pictureBo3.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p - 2]);
+                pictureBo2.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p - 3]);
+
+                pictureBox3.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p - 2]);
+                pictureBox2.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p-3]);
+
+                p = p - 1;
+                label1.Text = p + "";
+                label3.Text = (p - 1) + "";
+                for (int i = 0; i < 300; i++)
+                {
+                    pictureBox4.Height = pictureBox4.Height - (int)(i * 0.01);
+                    pictureBox4.Top = pictureBox4.Top - (int)(i * 0.001);
+
+                    pictureBo4.Height = pictureBo4.Height - (int)(i * 0.01);
+                    pictureBo4.Top = pictureBo4.Top - (int)(i * 0.001);
+                    Application.DoEvents();
+                }
+                for (int i = 0; i < 300; i++)
+                {
+                    pictureBox3.Height = (int)(pictureBox3.Height + (int)(i * 0.01));
+                    pictureBox3.Top = (int)(pictureBox3.Top - (int)(i * 0.01));
+
+                    pictureBo3.Height = (int)(pictureBo3.Height + (int)(i * 0.01));
+                    pictureBo3.Top = (int)(pictureBo3.Top - (int)(i * 0.01));
+                    Application.DoEvents();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (p < Program.imagecount) {
+                
+                if (p % 2 == 0)
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox1.Height = 300;
+                    pictureBox1.Top = 50;
+                    pictureBox3.BringToFront();
+                    pictureBox4.BringToFront();
+
+                    pictureBo1.Visible = true;
+                    pictureBo1.Height = 300;
+                    pictureBo1.Top = 50;
+                    pictureBo3.BringToFront();
+                    pictureBo4.BringToFront();
+
+                    pictureBo1.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p]);
+                    pictureBo4.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p - 1]);
+
+                    pictureBox1.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p]);
+                    pictureBox4.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p - 1]);
+
+                    p = p + 1;
+                    label1.Text = p + "";
+                    label3.Text = (p - 1) + "";
+                    for (int i = 0; i < 300; i++)
+                    {
+                        pictureBox3.Height = pictureBox3.Height - (int)(i * 0.01);
+                        pictureBox3.Top = pictureBox3.Top + (int)(i * 0.01);
+
+                        pictureBo3.Height = pictureBo3.Height - (int)(i * 0.01);
+                        pictureBo3.Top = pictureBo3.Top + (int)(i * 0.01);
+                        Application.DoEvents();
+                    }
+                    
+                    pictureBox4.Height = 0;
+                    pictureBox4.Top = 360;
+
+                    pictureBo4.Height = 0;
+                    pictureBo4.Top = 360;
+
+                    for (int i = 0; i < 300; i++)
+                    {
+                        pictureBox4.Height = pictureBox4.Height + (int)(i * 0.01);
+                        pictureBox4.Top = pictureBox4.Top + (int)(i * 0.001);
+
+                        pictureBo4.Height = pictureBo4.Height + (int)(i * 0.01);
+                        pictureBo4.Top = pictureBo4.Top + (int)(i * 0.001);
+                        Application.DoEvents();
+                    }
+                    pictureBox4.Visible = true;
+                    pictureBo4.Visible = true;
+                }
+                else {
+                    
+
+                    pictureBo3.Visible = true;
+                    pictureBo3.Height = 300;
+                    pictureBo3.Top = 50;
+                    pictureBo1.BringToFront();
+                    pictureBo2.BringToFront();
+                    pictureBox3.Visible = true;
+                    pictureBox3.Height = 300;
+                    pictureBox3.Top = 50;
+                    pictureBox1.BringToFront();
+                    pictureBox2.BringToFront();
+
+                    pictureBox3.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p]);
+                    pictureBox2.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p - 1]);
+                    
+                    pictureBo3.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p]);
+                    pictureBo2.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p - 1]);
+
+                    
+                    p = p + 1;
+                    label1.Text = p + "";
+                    label3.Text = (p-1) + "";
+                    for (int i = 0; i < 300; i++)
+                    {
+                        
+                        Application.DoEvents();
+                        pictureBox1.Height = pictureBox1.Height - (int)(i * 0.01);
+                        pictureBox1.Top = pictureBox1.Top + (int)(i * 0.01);
+
+                        pictureBo1.Height = pictureBo1.Height - (int)(i * 0.01);
+                        pictureBo1.Top = pictureBo1.Top + (int)(i * 0.01);
+                        Application.DoEvents();
+                    }
+                    pictureBo2.Height = 0;
+                    pictureBo2.Top = 360;
+                    pictureBox2.Height = 0;
+                    pictureBox2.Top = 360;
+
+
+
+                    for (int i = 0; i < 300; i++)
+                    {
+                        pictureBox2.Height = pictureBox2.Height + (int)(i * 0.01);
+                        pictureBox2.Top = pictureBox2.Top + (int)(i * 0.001);
+
+                        pictureBo2.Height = pictureBo2.Height + (int)(i * 0.01);
+                        pictureBo2.Top = pictureBo2.Top + (int)(i * 0.001);
+                        Application.DoEvents();
+                    }
+                }
+
+
+
+                if (p % 2 == 0)
+                {
+                    pictureBo3.Visible = true;
+                    pictureBo3.Height = 300;
+                    pictureBo3.Top = 50;
+                    pictureBo4.Height = 0;
+                    pictureBo4.Top = 360;
+                    pictureBox3.Visible = true;
+                    pictureBox3.Height = 300;
+                    pictureBox3.Top = 50;
+                    pictureBox4.Height = 0;
+                    pictureBox4.Top = 360;
+
+                }
+                else
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox1.Height = 300;
+                    pictureBox1.Top = 50;
+                    pictureBox2.Height = 0;
+                    pictureBox2.Top = 360;
+
+                    pictureBo1.Visible = true;
+                    pictureBo1.Height = 300;
+                    pictureBo1.Top = 50;
+                    pictureBo2.Height = 0;
+                    pictureBo2.Top = 360;
+                }
+
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            pictureBox1.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\" + Program.imagename[p]);
+            pictureBo1.Image = System.Drawing.Image.FromFile("C:\\Users\\ghksw\\Source\\Repos\\MLnetProject\\ObjectDetection\\assets\\images\\output\\" + Program.imagename[p]);
+
+            p = 1;
+            label1.Text = p + "";
+            label3.Text = (p-1) + "";
+
+            for (int i = 0; i < Program.imagecount; i++)
+            {
+                Debug.WriteLine("파일이름" + Program.imagename[i]);
+            }
+
+        }
+    }
+}
